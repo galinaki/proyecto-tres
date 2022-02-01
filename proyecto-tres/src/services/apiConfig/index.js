b9a008e47c05895ff426483a01d58ed4372eda7d
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-02-01 10:22:41 -0500

    index

--- proyecto-tres/src/services/apiConfig/index.js
+++ proyecto-tres/src/services/apiConfig/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { response } from "express";
 
 const baseURL = "https://jan27lesson.herokuapp.com/api/users";
 const signupURL = "https://jan27lesson.herokuapp.com/api/signup"
@@ -7,3 +8,48 @@ const logoutURL = "https://jan27lesson.herokuapp.com/api/logout";
 const deleteURL = "https://jan27lesson.herokuapp.com/api/delete:id";
 const updateUserURL = "https://jan27lesson.herokuapp.com/api/update/:userName";
 const findUserByIDURL = "https://jan27lesson.herokuapp.com/api/user/:id";
+
+export const existingUserHandler = (token) =>
+  axios({
+    url: `${baseURL}`,
+    headers: { Authorization: `${token}` },
+  })
+    .then((response) => {
+      return response;
+    });
+
+export const currentUserHandler = (token, userName) =>
+  axios({
+    url: `${baseURL}/currentUser/${userName}`,
+    headers: { Authorization: `${token}` },
+  })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+export const signUpHandler = (props) =>
+  axios({
+    method: "post",
+    url: `${signUpHandler}`,
+    data: props,
+  })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return error;
+    });
+    
+export const logOutHandler = () =>
+  axios({
+    url: `${logoutURL}`,
+  })
+    .then((response) => {
+      console.log(error)
+    })
+    .catch((error) => {
+      return error;
+    });
