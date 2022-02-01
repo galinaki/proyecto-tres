309903c3684ced9c5a20f4bcaf2a856413f87d66
Galina Kishirova <galinakishirova@Galinas-MacBook-Pro.local>
2022-01-31 22:00:56 -0500

    URL set up

---
+++ proyecto-tres/src/services/apiConfig/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const baseURL = "https://jan27lesson.herokuapp.com/api/users";
+const signupURL = "https://jan27lesson.herokuapp.com/api/signup"
+const loginURL = "https://jan27lesson.herokuapp.com/api/login";
+const logoutURL = "https://jan27lesson.herokuapp.com/api/logout";
+const deleteURL = "https://jan27lesson.herokuapp.com/api/delete:id";
+const updateUserURL = "https://jan27lesson.herokuapp.com/api/update/:userName";
+const findUserByIDURL = "https://jan27lesson.herokuapp.com/api/user/:id";
