358d7fb24b98d11bd051692e687e57a35680c616
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-03 09:52:41 -0500

    fixeddd

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,25 +1,20 @@
 import express from "express";
 import { getReviews, createReview, deleteReview, updateReview } from "../controllers/user/reviewController.js";
 import {addCourse,getCourse,fetchAllCourses} from "../controllers/user/addCourse.js";
-<<<<<<< HEAD
-import { fetchAllUsers, updateUser } from "../controllers/user/userController.js";
-import  defaultController  from "../controllers/defaultController.js"
-=======
 import { fetchAllUsers, updateUser, deleteUser } from "../controllers/user/userController.js";
 import defaultController from "../controllers/defaultController.js"
->>>>>>> 9a5874ba0ce7279cafe515aa5cd9cf8210b56bb0
 import {signUpUser,loginUser,logoutUser} from "../controllers/auth/authController.js"
 import cors from "cors"
 // import { sign } from "jsonwebtoken";
 // import { authRequired } from "../controllers/auth/authController.js"
 
 const Router = express.Router();
-// const options={
-//   "origin": "*",
-//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   "preflightContinue": true,
-//   "optionsSuccessStatus": 204
-// }
+const options={
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": true,
+  "optionsSuccessStatus": 204
+}
 //Home Route
 Router.get("/",cors(), defaultController)
   
@@ -38,12 +33,12 @@ Router.get("/",cors(), defaultController)
   //Update user
   .put("/update/:userName",cors(), updateUser)
 
-  
+
   //Delete user 
   .delete("/delete/:id",cors(), deleteUser)
   
 //Get a course
-  .get("/course/:courseId",cors(), getCourse)
+  .get("/course/:courseId", getCourse)
   
 //Get all courses
 .get("/courses",cors(), fetchAllCourses)
