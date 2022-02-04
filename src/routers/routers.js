9cb59d67a8a13b22b53b957e6adb0819577e81d3
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-04 18:31:25 -0500

    fixed sizing

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,62 +1,62 @@
 import express from "express";
 import { getReviews, createReview, deleteReview, updateReview } from "../controllers/user/reviewController.js";
-import {addCourse,getCourse,fetchAllCourses} from "../controllers/user/addCourse.js";
+import { addCourse, getCourse, fetchAllCourses } from "../controllers/user/addCourse.js";
 import { fetchAllUsers, updateUser, deleteUser } from "../controllers/user/userController.js";
 import defaultController from "../controllers/defaultController.js"
-import {signUpUser,loginUser,logoutUser, authRequired} from "../controllers/auth/authController.js"
+import { signUpUser, loginUser, logoutUser, authRequired } from "../controllers/auth/authController.js"
 import cors from "cors"
 // import { sign } from "jsonwebtoken";
 // import { authRequired } from "../controllers/auth/authController.js"
 
 const Router = express.Router();
-const options={
+const options = {
   "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": true,
   "optionsSuccessStatus": 204
 }
 //Home Route
-Router.get("/",cors(), defaultController)
-  
+Router.get("/", cors(), defaultController)
+
   //Create/sign-up user 
-  .post("/signup",cors(), signUpUser)
+  .post("/signup", cors(), signUpUser)
 
   //Get all users
-  .get("/users",cors(),authRequired, fetchAllUsers)
+  .get("/users", cors(), authRequired, fetchAllUsers)
 
   //Login user
-  .post("/login",cors(), loginUser)
-  
+  .post("/login", cors(), loginUser)
+
   //Log out user
-  .get("/logout",cors(), logoutUser)
-  
+  .get("/logout", cors(), logoutUser)
+
   //Update user
-  .put("/update/:userName",cors(),authRequired, updateUser)
+  .put("/update/:userName", cors(), authRequired, updateUser)
 
- 
+
   //Delete user 
-  .delete("/delete/:id",cors(),authRequired, deleteUser)
-  
-//Get a course
-  .get("/courses/:courseId",cors(), getCourse)
-  
-//Get all courses
-.get("/courses",cors(), fetchAllCourses)
+  .delete("/delete/:id", cors(), authRequired, deleteUser)
 
-//Create course 
-.post("/addcourse",cors(),authRequired, addCourse)
+  //Get a course
+  .get("/courses/:courseId", cors(), getCourse)
+
+  //Get all courses
+  .get("/courses", cors(), fetchAllCourses)
+
+  //Create course 
+  .post("/addcourse", cors(), authRequired, addCourse)
 
 
   //Get all reviews
-.get("/reviews", getReviews)
+  .get("/reviews", getReviews)
 
   //Create review 
-.post("/courses/addreview/:courseid",cors(),authRequired, createReview)
+  .post("/courses/addreview/:courseid", cors(), authRequired, createReview)
 
-.put("/update/:userid/:id",cors(),authRequired, updateReview)
+  .put("/update/:userid/:id", cors(), authRequired, updateReview)
 
   //Delete review
-.delete("/delete/review/:reviewid",cors(),authRequired, deleteReview);
+  .delete("/delete/review/:reviewid", cors(), authRequired, deleteReview);
 
 //Add a coursegi
 export default Router;
